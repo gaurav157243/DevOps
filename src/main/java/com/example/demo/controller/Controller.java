337adfe50fca35bcaef633e1c11f691cbabb51f6
gaurav157243 <gaurav157243@gmail.com>
2024-08-10 16:35:26 +0530

    Update Controller.java

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloWorldController {
+public class Controller {
 
     @GetMapping("/hello")
     public String helloWorld() {
